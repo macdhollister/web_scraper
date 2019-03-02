b19d66153d405a1e6c934bebb4195fe603ba8169
macdh <macdhollister@gmail.com>
2019-03-02 00:18:47 -0800

    Added expiration time for articles

--- models/Article.js
+++ models/Article.js
@@ -17,6 +17,11 @@ const ArticleSchema = new Schema({
         type: String,
         required: true
     },
+    createdAt: { 
+        type: Date, 
+        default: Date.now,
+        expires: 604800 // expires after 1 week
+    },
     comment: [{
         type: Schema.Types.ObjectId,
         ref: "Comment"
