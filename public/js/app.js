95f1565c19c583ab0db348de55052384fa328e07
macdh <macdhollister@gmail.com>
2019-03-04 02:01:02 -0800

    Added frontend js to unsave articles and bring up comments modal

--- public/js/app.js
+++ public/js/app.js
@@ -13,3 +13,25 @@ $(document).on("click", ".saveArticle", function(e) {
         else alert("This article was already saved!");
     })
 });
+
+$(document).on("click", ".unsaveArticle", function(e) {
+    e.preventDefault();
+    $.post("/unsave", {
+        id: $(this).parent().attr("articleId")
+    })
+    .done(data => {
+        if (data) alert("Post unsaved!");
+        $(this).parent().parent().remove();
+    })
+})
+
+$(".modal-button").click(function() {
+    var target = $(this).data("target");
+    $("html").addClass("is-clipped");
+    $(target).addClass("is-active");
+});
+
+$(".delete").click(function() {
+    $("html").removeClass("is-clipped");
+    $(this).parent().parent().parent().removeClass("is-active");
+});
